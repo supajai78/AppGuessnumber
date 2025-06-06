cc4315252f4360f3a2f052900724c3d35dd3440f
344_10 <344_10@DESKTOP-O0TAAU8.mshome.net>
2025-06-06 14:07:44 +0700

    sucees commmit

--- src/appguessnumber/AppGuessNumber.java
+++ src/appguessnumber/AppGuessNumber.java
@@ -3,18 +3,44 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package appguessnumber;
-
+import java.util.Random;
+import java.util.Scanner;
 /**
  *
  * @author 344_10
  */
 public class AppGuessNumber {
 
-    /**
+    /** 
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int numRandom;
+        int numGuess;
+        int count = 0;
+        Random r = new Random();
+        Scanner scan = new Scanner(System.in);
+        numRandom = r.nextInt(10);
+        System.out.println("number is "+numRandom);
+        //System.out.println("compari : "+ (numRandom==numGuess));
+        
+        while (count <= 3){
+        System.out.print("Enter you Guess : ");
+        numGuess = scan.nextInt();
+        if(numGuess==numRandom){
+            System.out.println("you win"); 
+            break;
+        }else if(numGuess>numRandom){
+            System.out.println("<<<<< decrement" + (count++));
+       
+        }else if (numGuess<numRandom){
+            System.out.println("Increment >>>>>" + (count++));
+        }
+         
+    
+     }if (count >= 3)
+        System.out.println("You lose !!");
+        
     }
     
 }
